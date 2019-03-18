4e6848d9f67383a495ed56d75f7831fd4bc8092d
Andreas Sahle <andreas.sahle@gmail.com>
2019-03-18 15:44:29 +0100

    chore: routing with pageJS

--- packages/layout-router/src/layout-router.ts
+++ packages/layout-router/src/layout-router.ts
@@ -1,50 +1,66 @@
 import { IGraphqlClient } from '@microfront/graphql-client';
 import gql from 'graphql-tag';
+import page from 'page';
 
 export interface ILayout {
-  [key: string]: string;
+  [key: string]: Array<{ [key: string]: any }>;
 }
 
 export interface ILayoutRouter {
-  loadLayout: () => Promise<any>;
+  loadLayout: (q: string) => Promise<any>;
+  initRouter: (layout: ILayout) => void;
+  renderPageRoute: (pageDefinition: { [key: string]: any }) => void;
 }
+
+const layoutQueryString = `
+query {
+  allLayouts {
+    name
+    path
+    grid {
+      id
+      regions {
+        id
+        name
+        content {
+          id
+          name
+          team
+          endpoint
+        }
+      }
+    }
+  }
+}
+`;
 
 export class LayoutRouter implements ILayoutRouter {
   private client: IGraphqlClient;
-  // @ts-ignore
-  private layout: ILayout;
-  constructor(client: IGraphqlClient) {
+  constructor(client: IGraphqlClient, queryString?: string) {
     this.client = client;
-    this.loadLayout().then(layout => (this.layout = layout));
+    const query = queryString || layoutQueryString;
+    this.loadLayout(query).then(layout => this.initRouter(layout.data));
   }
-  public async loadLayout() {
+  public async loadLayout(queryString: string) {
     const result = await this.client.query({
       query: gql`
-        query {
-          allLayouts {
-            name
-            path
-            grid {
-              id
-              regions {
-                id
-                name
-                content {
-                  id
-                  name
-                  team
-                  endpoint
-                }
-              }
-            }
-          }
-        }
+        ${queryString}
       `,
     });
     // tslint:disable no-console
     console.log(result);
-    return {
-      foo: 'bar',
-    };
+    return result.data;
+  }
+  public initRouter(layout: ILayout) {
+    layout.allLayouts.forEach((pageDefinition: { [key: string]: any }) => {
+      page(pageDefinition.path, () => this.renderPageRoute(pageDefinition));
+    });
+    page.show('/');
+  }
+  public renderPageRoute(pageDefinition: { [key: string]: any }) {
+    const pageEl = window.document.createElement('microfront-router-page');
+    pageEl.dataset.path = pageDefinition.path;
+    window!.document!.querySelector('body')!.innerHTML = '';
+    window!.document!.querySelector('body')!.append(pageEl);
   }
 }
